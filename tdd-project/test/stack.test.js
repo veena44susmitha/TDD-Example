bf9b2a7c7087d074aabfe2a8fd1d030012d49fad
veena44susmitha <veena44susmitha@gmail.com>
2023-07-29 14:21:46 +0530

    TDD commit

---
+++ tdd-project/test/stack.test.js
@@ -0,0 +1,40 @@
+class Stack {
+    constructor() {
+      this.top = -1;
+      this.items = {};
+    }
+  
+    get peek() {
+      return this.items[this.top];
+    }
+  
+    push(value) {
+      this.top += 1;
+      this.items[this.top] = value;
+    }
+  }
+  
+  describe('My Stack', () => {
+    let stack;
+  
+    beforeEach(() => {
+      stack = new Stack();
+    });
+  
+    it('is created empty', () => {
+      expect(stack.top).toBe(-1);
+      expect(stack.items).toEqual({});
+    });
+  
+    it('can push to the top', () => {
+      stack.push('5R');
+      expect(stack.top).toBe(0);
+      expect(stack.peek).toBe('5R');
+  
+      stack.push('8E');
+      expect(stack.top).toBe(1);
+      expect(stack.peek).toBe('8E');
+    });
+  
+    it.todo('can pop off');
+  });
